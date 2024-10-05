933171710e31a3057decc25898584f0d0bbd4f6f
KHPROTTASHA <111645444+KHPROTTASHA@users.noreply.github.com>
2024-10-05 15:17:42 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// function idFchange() {
+//     document.getElementById('demoid1').innerHTML = "we love my bangladesh"
+// }
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+function my_fun(value,index,array) {
+   console.log(value);
+   console.log(index);
+   console.log(array)
+}
+let bd = fruits.forEach(my_fun)
+console.log(bd)
+
+
